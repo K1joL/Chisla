4374ba877567dfae2f1db9f1a2f7aaf1aaf0bd23
KijoL <kzk19@mail.ru>
2022-03-17 20:05:25 +0300

    Добавьте файлы проекта.

---
+++ Chisla.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+void simple(int n)
+{
+	int i = 1, number = 2, sq;
+	while (i <= n)
+	{
+		if (i == 1)
+		{
+			cout << number << ' ';
+			number++;
+			i++;
+		}
+		int c;
+		c = 0;
+		sq = (int)sqrt(number);
+		for (int k = 2; k <= sq; k++)
+		{
+			if (number % k == 0) 
+			{
+				c++;
+				number++;
+				break;
+			}
+		}
+		if ((c == 0))
+		{
+			cout << number << ' ';
+			number++;
+			i++;
+		}
+	}
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int n;
+	char exit;
+	while (true)
+	{
+		cout << "\nВведите количество чисел\n";
+		cin >> n;
+		simple(n);
+		cout << '\n';
+
+		cout << "Если хотите выйти из программы введите 'x': ";
+		cin >> exit;
+		if (exit == 'x')
+		{
+			break;
+		}
+	}
+	return 0;
+}
